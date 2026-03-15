3365715e40d26d4f6cd5d985402670244b970b94
Ubuntu <ubuntu@ip-172-31-27-19.ec2.internal>
2026-03-15 11:12:22 +0000

    Updated version to 2.0

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def home():
     return jsonify({
         "message": "Jenkins CI/CD Pipeline Working!",
-        "version": "1.0",
+        "version": "2.0",
         "status": "deployed"
     })
 
